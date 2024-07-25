ffc69b39eb004bbd178a6014c13a128a62d0ef38
tokiniaina <hei.tokiniaina@gmail.com>
2024-07-25 12:17:04 +0300

    test: add test for rendering a list of patrimoines

---
+++ src/admin/patrimoinesListpatrimoineList.cy.tsx
@@ -0,0 +1,7 @@
+import patrimoineList from './patrimoinesList';
+
+describe('<patrimoineList />', () => {
+  it('should render the list of patrimoines', () => {``
+    cy.mount(patrimoineList )
+  })
+})
